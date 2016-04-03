f1dd3590978292348b6eb715d29163dca0ee80e3
james <lengjiabing@gmail.com>
2016-04-03 19:43:04 +0800

    今天没好好写代码

---
+++ sort_algorithms/InsertSort.c
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: InsertSort.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年04月02日 星期六 15时46分11秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+void
+InsertionSort( ElementType A[  ], int N )
+{
+    int j, P;
+
+    Element Type Temp;
+    for ( P = 1; P < N; P ++ )
+    {
+        Temp = A[ P ];
+        for( j = P; j > 0 && A[ j - 1 ] > Temp; j -- )
+        {
+            A[ j ] = A[ j - 1 ];
+        }
+    }
+}
